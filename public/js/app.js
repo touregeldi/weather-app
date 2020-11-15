89dadd859bc66aef5bb94bee25308251fd7b9a15
touregeldi <tore.kabi@gmail.com>
2020-11-15 16:24:02 +0500

    frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,24 @@
-fetch('http://localhost:9000/weather?address=atyrau').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:9000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = 'Location not found. Try again!'
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = `It is ${data.forecast.temperature} degrees out. But it feels like ${data.forecast.feelslike} degrees out.`
+            }
+        })
     })
+
+
 })
